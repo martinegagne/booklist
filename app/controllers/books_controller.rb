2506696d84731acd4e38276f84308e276eda8ea3
Martine Gagne <martine.e.gagne@gmail.com>
2017-08-09 14:54:14 -0400

    populate index html view page with books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,9 @@ class BooksController < ApplicationController
     respond_to do |format|
       format.html
       format.text
+      format.csv do
+        self.generate_csv(book_list)
+      end
     end
   end
 
